59aa14defa9dae3ec415a02f0354a91a7918b5f3
smease12 <scmease93@gmail.com>
2024-01-21 17:56:50 -0500

    Print text entered into searchTextField

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -19,6 +20,9 @@ class WeatherViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
 }
 
