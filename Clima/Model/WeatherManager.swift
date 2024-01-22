fe3d123ddb11af646dbce43e845f4eb3be06d4c2
smease12 <scmease93@gmail.com>
2024-01-21 20:09:40 -0500

    Add weatherManager struct with url for weather info by city

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Serena  on 1/21/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager{
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=427923459170229f720342da44724965&units=metric"
+    
+    func fetchWeater(cityName: String){
+        let urlString = "\(weatherURL)&q\(cityName)"
+    }
+    
+}
