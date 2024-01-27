beb41eac89b728c29d0362a3f6425c71f9d7ca45
smease12 <scmease93@gmail.com>
2024-01-26 20:42:42 -0500

    Add weather struct to weather data to parse json for weather description and print on search city

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -44,7 +44,7 @@ struct WeatherManager{
         let decoder = JSONDecoder()
         do{
            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
         } catch{
             print(error)
         }
