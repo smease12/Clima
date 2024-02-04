df6d2ab06bab14ea12a95527fb8734f77434fa4c
smease12 <scmease93@gmail.com>
2024-02-03 21:08:10 -0500

    Update city label based on city searched

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -65,6 +65,7 @@ extension WeatherViewController: WeatherManagerDelegate{
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
             self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
         }
     }
     
